4030db4320858198294f46ca8ce2d6663a758a06
Sean Blanton <seanblanton@gmail.com>
2017-01-16 15:45:56 -0800

    linting

--- index.js
+++ index.js
@@ -1,96 +1,82 @@
 'use strict';
 
-const chroma = require('chroma-js')
-const fs = require('fs')
-const Task = require('data.task')
-const { lumSet, hueSet } =  require('./config')
+const chroma = require('chroma-js');
+const fs = require('fs');
+const Task = require('data.task');
+const { lumSet, hueSet } = require('./config');
 
-const colors = {}
+const colors = {};
+
+// returns a set of values mapped from one func to another
+// from https://github.com/koenbok/Framer/blob/master/framer/Utils.coffee
+const modulate = (value, [fromLow, fromHigh], [toLow, toHigh]) =>
+  toLow + (((value - fromLow) / (fromHigh - fromLow)) * (toHigh - toLow));
 
 /*
  * return evenly spaced luminance values as floats
  * this modulates to keep the ends from getting too light or dark to be useful
  */
 const stepsToFloat = options =>
-  Array.from({length: options.luminance}, (n, i) =>
-    (i + 0.5 )/ 10).reverse()
-  .map( s => modulate(s, [0, 1], [options.start, options.end]))
-
-// returns a set of values mapped from one func to another
-// from https://github.com/koenbok/Framer/blob/master/framer/Utils.coffee
-const modulate = (value, [fromLow = rangeA, fromHigh = rangeB], [toLow = rangeC, toHigh = rangeD]) =>
-  toLow + (((value - fromLow) / (fromHigh - fromLow)) * (toHigh - toLow))
+  Array.from({ length: options.luminance }, (n, i) =>
+    (i + 0.5) / 10).reverse()
+  .map(s => modulate(s, [0, 1], [options.start, options.end]));
 
 // shift color on quadradic curve
 // TODO: error handling
 const shiftColor = (color, s, index, shift) =>
   chroma(s)
-  .set('hsl.h', chroma(color).get('hsl.h') - ( (index * (index * 2) ) * (shift * 0.01)) ).hex()
+  .set('hsl.h', chroma(color).get('hsl.h') - ((index * (index * 2)) * (shift * 0.01))).hex();
 
 // shift saturation on logorithmic curve
 // TODO: error handling
 const shiftSat = (color, s, index, sat) =>
   chroma(s)
-  .saturate( ( ( sat * 0.1) * Math.log(index + 1)) )
-  .hex()
+  .saturate(((sat * 0.1) * Math.log(index + 1)))
+  .hex();
+
+// expand the color array to allow them to be accessed by name in the JSON
+const expandColors = (key, steps) => {
+  const resolvedColors = {};
+  steps
+  .map((step, index) => (resolvedColors[key + index] = steps[index]));
+  return resolvedColors;
+};
 
 // return an object of color steps for a given hue, with options
 // TODO: error handling
 const buildColor = (key, sat, shift) => {
-
-  const color = hueSet[key]
+  const color = hueSet[key];
 
   const steps = stepsToFloat(lumSet)
-  .map( (s, index) => chroma(color.hue).luminance(s).hex() )
-  .map ( (s, index) => sat   ?  shiftSat(color.hue, s, index, sat) : s )
-  .map ( (s, index) => shift ?  shiftColor(color.hue, s, index, shift) : s )
+  .map(s => chroma(color.hue).luminance(s).hex())
+  .map((s, index) => sat ? shiftSat(color.hue, s, index, sat) : s)
+  .map((s, index) => shift ? shiftColor(color.hue, s, index, shift) : s);
 
   // return values as incremented key value pair
-  const values = expandColors(key, steps)
+  const values = expandColors(key, steps);
 
-  colors[key] = values
-
-}
-
-// expand the color array to allow them to be accessed by name in the JSON
-// TODO: error handling
-const expandColors = (key, steps) => {
-  const resolvedColors = {}
-
-  for(var index in steps) {
-    const color = key+index
-    const value = steps[index]
-    resolvedColors[color] = value
-  }
-  return resolvedColors
-}
+  colors[key] = values;
+};
 
 // modify via config.js
-// TODO: error handling
 const buildPalette = (set) => {
-  for(var key in set) {
-    buildColor(key, set[key].sat, set[key].shift)
-  }
-  return colors
-}
+  Object.keys(set).forEach((key) => {
+    buildColor(key, set[key].sat, set[key].shift);
+  });
+  return colors;
+};
+
 
 const writeFile = (filename, contents) =>
-  new Task( (rej, res) =>
-    fs.writeFile(filename, contents, (err, success)  => err ? rej(err) : res(success) ))
+  new Task((rej, res) =>
+    fs.writeFile(filename, contents, (err, success) => err ? rej(err) : res(success)));
 
 // refactor to be a task. run as app().fork()
 const app = () => {
-  buildPalette(hueSet)
+  buildPalette(hueSet);
   writeFile('colorPalette.json', JSON.stringify(colors, null, 4))
   .fork(e => console.log(e),
-        s => console.log("Success"))
-}
+        s => console.log('Success'));
+};
 
-app()
-
-
-
-
-
-
-
+app();
